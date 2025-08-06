fc039616e3f5833410a59acf91e7e5e10748575f
Peter Pham <pttp15122002@gmail.com>
2025-08-06 17:56:08 +1000

    feat: current date and input for staff name

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,29 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Stack, Typography } from '@mui/material'
-import z from 'zod'
-import { Counter } from '~/components/Counter'
+import { createFileRoute } from "@tanstack/react-router";
+import { Grid, Stack, TextField, Typography } from "@mui/material";
+import z from "zod";
+import { Counter } from "~/components/Counter";
+import useGetTodayDate from "~/hooks/useGetTodayDate";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   validateSearch: z.object({
     count: z.number().optional(),
   }),
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
+  const currentDate = useGetTodayDate();
+
   return (
-    <Stack alignItems="center">
-      <Typography variant="h1" marginBlockEnd={4}>
-        Hello world!
-      </Typography>
-      <Counter />
+    <Stack direction={"column"} gap={2} alignItems="center">
+      <TextField
+        id="standard-basic"
+        label="Date"
+        variant="standard"
+        disabled
+        value={currentDate}
+      />
+      <TextField id="standard-basic" label="Staff Name" variant="standard" />
     </Stack>
-  )
+  );
 }
