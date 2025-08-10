3cb48a1c1e0eefc9d64dce0bf263e7850aa2196e
Peter Pham <pttp15122002@gmail.com>
2025-08-10 16:43:14 +1000

    refactor: split the form to a component

---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,35 @@
+import { Button, Stack, TextField } from "@mui/material";
+import useGetTodayDate from "~/hooks/useGetTodayDate";
+
+export default function SignUpForm() {
+  const currentDate = useGetTodayDate();
+  return (
+    <Stack
+      direction={"column"}
+      paddingInline={4}
+      marginBlock="auto"
+      gap={2}
+      alignItems="center"
+      justifyContent="center"
+      minHeight="75dvh"
+    >
+      <TextField
+        id="standard-basic"
+        label="Date"
+        variant="standard"
+        disabled
+        fullWidth
+        value={currentDate}
+      />
+      <TextField
+        fullWidth
+        id="standard-basic"
+        label="Staff Name"
+        variant="standard"
+      />
+      <Button fullWidth sx={{ marginTop: 2 }} variant="contained">
+        Next Step
+      </Button>
+    </Stack>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,45 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Button, Stack, TextField } from "@mui/material";
-import z from "zod";
-import useGetTodayDate from "~/hooks/useGetTodayDate";
+import SignUpForm from "~/components/SignUpForm";
 
 export const Route = createFileRoute("/")({
-  validateSearch: z.object({
-    count: z.number().optional(),
-  }),
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const currentDate = useGetTodayDate();
-
   return (
-    <Stack
-      direction={"column"}
-      paddingInline={4}
-      marginBlock="auto"
-      gap={2}
-      alignItems="center"
-      justifyContent="center"
-      minHeight="75dvh"
-    >
-      <TextField
-        id="standard-basic"
-        label="Date"
-        variant="standard"
-        disabled
-        fullWidth
-        value={currentDate}
-      />
-      <TextField
-        fullWidth
-        id="standard-basic"
-        label="Staff Name"
-        variant="standard"
-      />
-      <Button fullWidth sx={{ marginTop: 2 }} variant="contained">
-        Next Step
-      </Button>
-    </Stack>
+    <>
+      <SignUpForm />
+    </>
   );
 }
