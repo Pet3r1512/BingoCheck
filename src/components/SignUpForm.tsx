07e21577796d4622db295baf51fa11048e0e51f0
Peter Pham <pttp15122002@gmail.com>
2025-08-13 09:08:18 +1000

    ops: declare mutation from client

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,4 +1,7 @@
+import { convexQuery } from "@convex-dev/react-query";
 import { Button, Stack, TextField } from "@mui/material";
+import { api } from "convex/_generated/api";
+import { useMutation } from "convex/react";
 import { useState } from "react";
 import useGetTodayDate from "~/hooks/useGetTodayDate";
 
@@ -6,6 +9,17 @@ export default function SignUpForm() {
   const [staffName, setStaffName] = useState<string>("");
   const [totalBox, setTotalBox] = useState<number>(0);
   const currentDate = useGetTodayDate();
+
+  const deliveryMutation = useMutation(api.delivery.createDelivery);
+
+  const handleClick = () => {
+    deliveryMutation({
+      date: currentDate,
+      staffName: staffName,
+      totalBox: totalBox,
+    });
+  };
+
   return (
     <Stack
       direction={"column"}
