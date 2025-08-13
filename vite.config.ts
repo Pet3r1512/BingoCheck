3d9bcc01a0aae30897f4c0e47181db69c070caaa
Peter Pham <pttp15122002@gmail.com>
2025-08-13 10:34:44 +1000

    add: config for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3000
   },
   plugins: [
     tsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-    tanstackStart(),
+    tanstackStart({
+      target: "vercel"
+    }),
   ],
 })
