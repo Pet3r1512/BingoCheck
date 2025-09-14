1cc53f5cd652a5006e0b9edf4d42a5cf7ba5e955
Peter Pham <pttp15122002@gmail.com>
2025-09-14 20:45:24 +1000

    add: button for each index

---
+++ src/components/checklist/Checkbox.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@mui/material";
+import { useState } from "react";
+
+export default function Checkbox({ number }: { number: number }) {
+  const [currVariant, setCurrVariant] = useState<
+    "outlined" | "text" | "contained"
+  >("outlined");
+  return (
+    <Button
+      onClick={() => {
+        setCurrVariant("contained");
+      }}
+      variant={currVariant}
+    >
+      {number}
+    </Button>
+  );
+}
