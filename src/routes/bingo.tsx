e8fb0ce57debcc35c0a0c0c27edee32e7137736b
Peter Pham <pttp15122002@gmail.com>
2025-09-14 20:45:12 +1000

    add: list for checking index of stock

--- src/routes/bingo.tsx
+++ src/routes/bingo.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import CustomizedAccordions from "~/components/checklist/Accordion";
 
 export const Route = createFileRoute("/bingo")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/bingo"!</div>;
+  return (
+    <div>
+      <CustomizedAccordions />
+    </div>
+  );
 }
